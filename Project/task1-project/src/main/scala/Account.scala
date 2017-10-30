9641a5e20e27a81401e108c2d9df4140bdeca37c
hallvardd <hallvste@stud.ntnu.no>
2017-10-30 22:28:57 +0100

    implemented basic functionallity of Account and Bank

--- Project/task1-project/src/main/scala/Account.scala
+++ Project/task1-project/src/main/scala/Account.scala
@@ -1,5 +1,22 @@
+import exceptions.{IllegalAmountException, NoSufficientFundsException}
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+  private var balance = initialBalance
+  def withdraw(amount: Double): Unit = {
+    if(amount < 0){
+      throw new IllegalAmountException(s"$amount")
+    }
+    if(amount > balance){
+      throw new NoSufficientFundsException(s"$balance")
+    }
+    else balance = balance - amount
+
+  }
+  def deposit(amount: Double): Unit = {
+    if (amount > 0) {
+      balance = balance + amount
+    }
+    else throw new IllegalAmountException(s"$amount")
+  }
+  def getBalanceAmount: Double = balance
 }
